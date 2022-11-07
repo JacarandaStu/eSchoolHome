d4fbc5054917aa5bba1902bd16396bf390b8840a
JacarandaStu <110702688+JacarandaStu@users.noreply.github.com>
2022-11-07 22:35:14 +0800

    Add files via upload
    代传_高_主页与我的模块代码

---
+++ pages/student/my/orders/orders.js
@@ -0,0 +1,96 @@
+// pages/my/orders/orders.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    select: false,
+    tihuoWay: '父亲',
+    showModal: false,
+    head:  "/images/touxiang.jpg",
+  },
+  // 点击更换手机相册或者电脑本地图片   
+  headimage: function () {
+    var  _this = this;
+     wx.chooseImage({
+       count: 1, // 默认9     
+       sizeType: ['original', 'compressed'],
+      // 指定是原图还是压缩图，默认两个都有     
+       sourceType: ['album', 'camera'],
+      // 指定来源是相册还是相机，默认两个都有   
+       success: function (res) {   
+         _this.setData({
+           head: res.tempFilePaths
+        })
+      }
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+  bindShowMsg() {
+    this.setData({
+        select:!this.data.select
+    })
+  },
+  mySelect(e) {
+    var name = e.currentTarget.dataset.name
+    this.setData({
+        tihuoWay: name,
+        select: false
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ pages/teacher/home/home.js
@@ -0,0 +1,100 @@
+var app = getApp();
+Page({
+  data: {
+    username: wx.getStorageSync('name'),
+    swiper_img: [
+      "/images/home1.png",
+    ],
+    authorized: false,
+    icon: [{
+        id: 1,
+        name: "request_leave",
+        img: "/images/lease00.png",
+        text: "请假管理"
+      },
+      {
+        id: 2,
+        name: "sign",
+        img: "/images/daka01.png",
+        text: "考勤打卡"
+      },
+      {
+        id: 3,
+        name: "shop",
+        img: "/images/shop01.png",
+        text: "文件收集"
+      },
+      
+    ]
+  },
+
+  onLoad: function (options) {
+    var that = this;
+    that.getSwiper();
+    that.getHot();
+    app.getOpenid();
+    setTimeout(function () {
+      //app.findHaveSign();
+    }, 500)
+  },
+  onShow: function (res) {
+    var that = this;
+    var authorized = wx.getStorageSync('authorized');
+    if (authorized) {
+      that.setData({
+        authorized: true
+      })
+    }
+  },
+  authorize: function (res) {
+    app.authorize();
+    setTimeout(() => {
+      this.onShow();
+    }, 3000);
+    //首页授权
+    // wx.getUserInfo({
+    //   success: function (res) {
+    //     console.log(res)
+    //     wx.setStorageSync('userInfo', res.userInfo)
+    //   }
+    // })
+  },
+  getSwiper: function (res) {
+    var that = this;
+    wx.request({
+      url: app.globalData.host + 'getSwiper.php',
+      success: function (res) {
+        console.log(res)
+        that.setData({
+          swiper_img: res.data
+        })
+      }
+    })
+  },
+  getHot: function (res) {
+    var that = this;
+    wx.request({
+      url: app.globalData.host + 'getHot.php',
+      success: function (res) {
+        console.log(res)
+        that.setData({
+          hotlist: res.data
+        })
+      }
+    })
+  },
+  go: function (res) {
+    var index = res.currentTarget.id;
+    if (index < 6) {
+      var name = this.data.icon[index - 1].name;
+      console.log('/pages/' + name + '/' + name)
+      wx.navigateTo({
+        url: '/pages/' + name + '/' + name,
+      })
+    } else {
+      wx.switchTab({
+        url: '/pages/teacher/my/my.wxml',
+      })
+    }
+  }
+})
